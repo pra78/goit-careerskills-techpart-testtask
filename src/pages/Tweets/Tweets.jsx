ffefdc83ff7544dab0ea7a5c871882d9f9e5df47
pra78 <66877961+pra78@users.noreply.github.com>
2023-06-02 18:40:22 +0300

    logic without filter and number format

--- src/pages/Tweets/Tweets.jsx
+++ src/pages/Tweets/Tweets.jsx
@@ -1,40 +1,36 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { followTweet, getTweets } from "service/api";
+import { NumericFormat } from 'react-number-format';
+import { TextField } from '@mui/material';
 
 const Tweets = () => {
     const [tweets, setTweets] = useState([]);
     const [page, setPage] = useState(1);
 
-    
-
     useEffect(() => {
         const fetchTweets = async (page) => {
-            try {
-                const data = await getTweets(page);
-                setTweets(prev => ([...prev, ...data]));
-            } catch (error) {
-                console.log(error);
-            }
-        }
+            const {data} = await getTweets(page);
+            setTweets(prev => ([...prev, ...data]));
+        }    
         fetchTweets(page);
     }, [page]);
 
-    const handleFollowButtonClick = async (id) => {
-        if (tweets.isFollowed === true) {
-            setTweets(prev => ({ ...prev, followers: prev.followers - 1, isFollowed: false }));
-        } else {
-            setTweets(prev => ({ ...prev, followers: prev.followers + 1, isFollowed: true }));
-        }
-        try {
-            const data = await followTweet(id, tweets.isFollowed);
-            console.log(data);
-        } catch (error) {
-            console.log(error);
-        }
+    const handleFollowButtonClick = (id) => {
+        const idx = tweets.findIndex(el => (el.id === id));
+        tweets[idx].isFollowed === true ? followTweet(id, { isFollowed: false, followers: tweets[idx].followers - 1 }) : followTweet(id, { isFollowed: true, followers: tweets[idx].followers + 1 });
+        setTweets(prev => prev.map(el => el.id === id ? {
+            ...el,
+            isFollowed: !el.isFollowed,
+            followers: el.isFollowed ? el.followers - 1 : el.followers + 1,
+            }
+            : el));
+        
+        
     }
 
-    const handleLoadMoreButtonClick = () => {
+    const handleLoadMoreButtonClick = (event) => {
+        event.preventDefault();
         setPage(prev => prev + 1);
     }
 
@@ -43,11 +39,11 @@ const Tweets = () => {
             <h1>Tweets</h1>
             <NavLink to="/">Back</NavLink>
             <ul>
-                {tweets.map(({ id, userName, tweets, avatar, follwers }) => (<li key={id}>
+                {tweets.map(({ id, userName, tweets, avatar, followers, isFollowed }) => (<li key={id}>
                     <img src={avatar} alt={userName} />
                     <p>{tweets} TWEETS</p>
-                    <p>{follwers} FOLLOWERS</p>
-                    <button type="button" onClick={id => handleFollowButtonClick(id)}>FOLLOW</button>
+                    <p><NumericFormat thousandSeparator="," value={followers} customInput={TextField} /> FOLLOWERS</p>
+                    {isFollowed ? (<button type="button" onClick={() => handleFollowButtonClick(id)}>FOLLOWING</button>) : (<button type="button" onClick={() => handleFollowButtonClick(id)}>FOLLOW</button>)}
                 </li>))}
             </ul>
             <button type="button" onClick={handleLoadMoreButtonClick}>Load More</button>
--- src/service/api.js
+++ src/service/api.js
@@ -2,16 +2,20 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://647991e0a455e257fa6357f0.mockapi.io";
 
-axios.defaults.params = {
-  limit: 3,
+export const getTweets = async (page = 1) => {
+  try {
+    const data = await axios.get("/users", { params: { limit: 3, page } });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const getTweets = (page = 1) => {
-  return axios.get("/users", { params: { page } }).then((res) => res.data);
-};
-
-export const followTweet = (id, isFollowed) => {
-  return axios
-    .patch(`/users/${id}`, { params: { isFollowed } })
-    .then((res) => res.data);
+export const followTweet = async (id, tweet) => {
+  try {
+    const data = await axios.put(`/users/${id}`, { ...tweet });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
