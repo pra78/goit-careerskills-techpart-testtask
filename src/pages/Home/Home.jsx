ce381852d358f1f451119ec702e04cfd5e38e2e1
pra78 <66877961+pra78@users.noreply.github.com>
2023-06-03 21:05:03 +0300

    refactoring done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,9 +8,7 @@ const Home = () => {
             <NavLinkStyled to="/tweets" >Tweets</NavLinkStyled>
         </NavBarStyled>
         <TextPageStyled>
-
             <h4 align="center">A minimal tweet cards app with some interactivity when a button is clicked.</h4>
-
             <TechStackStyled align="center">
                 <a href="https://img.shields.io/badge/stack-React-blue">
                     <img src="https://img.shields.io/badge/stack-React-blue" alt="react" />
@@ -19,11 +17,8 @@ const Home = () => {
                     <img src="https://img.shields.io/badge/stack-JS-brightgreen" alt="JS" />
                 </a>
             </TechStackStyled>
-
             <TechStackStyled>Mockup: <a href="https://www.figma.com/file/zun1oP6NmS2Lmgbcj6e1IG/Test?node-id=0%3A1&t=VoiYTfiXggVItgVd-1">https://www.figma.com/file/zun1oP6NmS2Lmgbcj6e1IG/Test?node-id=0%3A1&t=VoiYTfiXggVItgVd-1</a></TechStackStyled>
-
             <PrintScreenStyled src="https://textbook.edu.goit.global/lms-career-homework/uk/img/image-1.jpg" alt="printscreen of the app" />
-
             <h3>Key Features</h3>
             <ul>
                 <li>Interactive - click Follow button => the button text is changed to Following.</li>
@@ -34,7 +29,6 @@ const Home = () => {
                 <li>The followers quantity is also set back (by minus 1).</li>
                 <li>Personal backend is attached to this app by UI-service mockapi.io.</li>
             </ul>
-
             <h3>How To Use</h3>
             <p>
                 To clone and run this application, you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
