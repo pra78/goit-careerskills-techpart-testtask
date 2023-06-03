1f44e0328f0c5db7150505787ca514c4301478b3
pra78 <66877961+pra78@users.noreply.github.com>
2023-06-03 21:23:04 +0300

    github routing bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Home from "pages/Home/Home";
 import Tweets from "pages/Tweets/Tweets";
-import { Navigate, Routes, Route } from "react-router-dom";
+import { Navigate, Route, HashRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <Routes>
+    <HashRouter>
       <Route path="/" element={<Home/>} />
       <Route index element={<Home />} />
       <Route path="tweets" element={<Tweets />} />
       <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    </HashRouter>
   );
 };
 
